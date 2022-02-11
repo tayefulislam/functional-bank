efd7ae4a4704fe79311fb8a79d525e972f089634
tayeful912 <kazitayefulislam12@gmail.com>
2022-02-12 02:49:35 +0600

    error handel done

--- js/banking.js
+++ js/banking.js
@@ -1,7 +1,7 @@
 
 //common function 
 
-
+// get the input value
 function input(inputId) {
 
     let inputValue = document.getElementById(inputId);
@@ -10,9 +10,11 @@ function input(inputId) {
     // let inputAmount = parseFloat(inputAmountText);
 
     let inputAmount = parseFloat(inputValue.value);
+
     inputValue.value = '';
 
     // let inputValue = parseFloat(document.getElementById(inputId).value)
+
     return inputAmount;
 
 
@@ -30,96 +32,169 @@ function calTotal(valueId, total) {
 }
 
 
+function getCurrentBalance() {
 
+    let mainBalance = document.getElementById('balance-total');
+
+    let mainBalanceText = mainBalance.innerText;
+    let mainBalanceAmount = parseFloat(mainBalanceText);
+
+    return mainBalanceAmount;
+}
+
+
+
+//update the balance 
+function updateBalance(getBalance, isAdd) {
+
+    let mainBalance = document.getElementById('balance-total');
+
+    // let mainBalanceText = mainBalance.innerText;
+    // let mainBalanceAmount = parseFloat(mainBalanceText);
+
+    let mainBalanceAmount = getCurrentBalance();
+
+    if (isAdd == true) {
+        mainBalance.innerText = mainBalanceAmount + getBalance;
+
+    }
+    else {
+        mainBalance.innerText = mainBalanceAmount - getBalance;
+
+    }
+    // mainBalance.innerText = mainBalanceAmount + getBalance;
+
+
+}
 
 
 // deposit and upadate balance 
 
 document.getElementById('deposit-btn').addEventListener('click', function () {
 
-    // get the input by button click step : 1
-
-    // get deposit input by call ta funtion
     let getDeposit = input('deposit-amount');
-
-    // let depositInput = document.getElementById('deposit-amount');
-    // let depositAmountText = depositInput.value;
-
-    // let depositAmount = parseFloat(depositAmountText);
-
-
-    //get the deposit balance step : 2
-
-    let getTotal = calTotal('deposit-total', getDeposit);
-    // let depositField = document.getElementById('deposit-total');
-
-    // let depositFieldText = depositField.innerText;
-
-    // let depositFieldAmount = parseFloat(depositFieldText);
-
-    // // set the input value(deposit amount) to as new deposite value 
-
-    // depositField.innerText = depositFieldAmount + getDeposit;
-
-    //update the balance 
-
-    let mainBalance = document.getElementById('balance-total');
-    let mainBalanceText = mainBalance.innerText;
-
-    let mainBalanceAmount = parseFloat(mainBalanceText);
-
-    mainBalance.innerText = mainBalanceAmount + getDeposit;
-
-
-    //clear the deposite input field
-    // depositInput.value = '';
+    if (getDeposit > 0) {
+        calTotal('deposit-total', getDeposit);
+        updateBalance(getDeposit, true);
+    }
 
 });
 
-
 // withdraw and update balance 
-//get the withdraw button
 document.getElementById('withdraw-btn').addEventListener('click', function () {
 
-
-    // let withdrawInput = document.getElementById('withdraw-amount');
-    // let withdrawInputText = withdrawInput.value;
-
-    // let withdrawInputAmount = parseFloat(withdrawInputText);
     let getWithdraw = input('withdraw-amount');
+    let currentBalance = getCurrentBalance();
 
-
-    // set withdraw main balance 
-
-    let getTotal = calTotal('withdraw-total', getWithdraw);
-
-    /* let withdrawMainBalance = document.getElementById('withdraw-total');
-
-    let withdrawMainBalanceText = withdrawMainBalance.innerText;
-
-    let withdrawMainAmount = parseFloat(withdrawMainBalanceText);
-
-    withdrawMainBalance.innerText = withdrawMainAmount + getWithdraw; */
-
-
-    // update balance 
-
-    let mainBalance = document.getElementById('balance-total');
-    let mainBalanceText = mainBalance.innerText;
-
-    let mainBalanceAmount = parseFloat(mainBalanceText);
-
-    console.log(mainBalanceAmount);
-
-    mainBalance.innerText = mainBalanceAmount - getWithdraw;
-
-
-    //clear the withdraw input field
-
-    // withdrawInput.value = '';
-
-
+    if (getWithdraw > 0 && getWithdraw <= currentBalance) {
+        let getTotal = calTotal('withdraw-total', getWithdraw);
+        updateBalance(getWithdraw, false);
+    }
 
 })
+
+// document.getElementById('deposit-btn').addEventListener('click', function () {
+
+//     let getDeposit = input('deposit-amount');
+//     let getTotal = calTotal('deposit-total', getDeposit);
+//     updateBalance(getDeposit, true);
+
+//     // get the input by button click step : 1
+
+//     // get deposit input by call ta funtion
+
+//     // let getDeposit = input('deposit-amount');
+
+//     // let depositInput = document.getElementById('deposit-amount');
+//     // let depositAmountText = depositInput.value;
+
+//     // let depositAmount = parseFloat(depositAmountText);
+
+
+//     //get the deposit balance step : 2
+
+//     // let getTotal = calTotal('deposit-total', getDeposit);
+//     // let depositField = document.getElementById('deposit-total');
+
+//     // let depositFieldText = depositField.innerText;
+
+//     // let depositFieldAmount = parseFloat(depositFieldText);
+
+//     // // set the input value(deposit amount) to as new deposite value
+
+//     // depositField.innerText = depositFieldAmount + getDeposit;
+
+//     // //update the balance
+
+//     // updateBalance(getDeposit, true);
+//     // let mainBalance = document.getElementById('balance-total');
+//     // let mainBalanceText = mainBalance.innerText;
+//     // let mainBalanceAmount = parseFloat(mainBalanceText);
+//     // mainBalance.innerText = mainBalanceAmount + getDeposit;
+
+
+//     //clear the deposite input field
+//     // depositInput.value = '';
+
+// });
+
+
+
+
+// withdraw and update balance
+//get the withdraw button
+
+
+// document.getElementById('withdraw-btn').addEventListener('click', function () {
+
+//     let getWithdraw = input('withdraw-amount');
+//     let getTotal = calTotal('withdraw-total', getWithdraw);
+//     updateBalance(getWithdraw, false);
+
+
+//     // let withdrawInput = document.getElementById('withdraw-amount');
+//     // let withdrawInputText = withdrawInput.value;
+
+//     // let withdrawInputAmount = parseFloat(withdrawInputText);
+//     // let getWithdraw = input('withdraw-amount');
+
+
+//     // set withdraw main balance
+
+//     // let getTotal = calTotal('withdraw-total', getWithdraw);
+
+//     /* let withdrawMainBalance = document.getElementById('withdraw-total');
+
+//     let withdrawMainBalanceText = withdrawMainBalance.innerText;
+
+//     let withdrawMainAmount = parseFloat(withdrawMainBalanceText);
+
+//     withdrawMainBalance.innerText = withdrawMainAmount + getWithdraw; */
+
+
+//     // update balance
+
+//     // updateBalance(getWithdraw, false);
+
+//     // updateBalance(-getWithdraw); jodi biyog korte chai tahole paramiter man ta pathanor age - minus mark ta dilei dilei kaj hoy .
+//     // tokhon ar funtion if else condition dite hoy na
+
+//     // let mainBalance = document.getElementById('balance-total');
+//     // let mainBalanceText = mainBalance.innerText;
+
+//     // let mainBalanceAmount = parseFloat(mainBalanceText);
+
+//     // console.log(mainBalanceAmount);
+
+//     // mainBalance.innerText = mainBalanceAmount - getWithdraw;
+
+
+//     //clear the withdraw input field
+
+//     // withdrawInput.value = '';
+
+
+
+// })
 
 
